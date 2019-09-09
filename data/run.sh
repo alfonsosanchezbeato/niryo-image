976cf59ca3e6b817bb91e570e953e895ad29d132
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-09-09 13:40:27 +0200

    Try to refresh niryo-sequence each 20 seconds

--- data/run.sh
+++ data/run.sh
@@ -10,12 +10,12 @@ print_system()
         tee /dev/kmsg /dev/console || true
 }
 
-service_dir=/writable/system-data/var/lib/configure-device
+#service_dir=/writable/system-data/var/lib/configure-device
 
 # Don't start again if we're already done
-if [ -e "$service_dir"/complete ]; then
-    exit 0
-fi
+# if [ -e "$service_dir"/complete ]; then
+#     exit 0
+# fi
 
 print_system "start"
 
@@ -25,24 +25,23 @@ until snap changes; do
     sleep 5
 done
 
-# If we have the assertion, create the user
+# If we have the assertion, create the user if not alreay there
 if [ "$(snap managed)" != "true" ] && [ -n "$(snap known system-user)" ]; then
     snap create-user --known --sudoer
     print_system "system user created"
 fi
 
 # Wait for all snaps to be available
-#snap wait system seed.loaded
+snap wait system seed.loaded
 
-# Apply network configuration
-#netplan generate
-#systemctl restart systemd-networkd.service
-# Remove old addresses - otherwise we might have problems if the dhcp server
-# gives different addresses to NM than those given to networkd.
-#ip address flush dev eth0
-#systemctl restart snap.network-manager.networkmanager.service
+# Force running config hook now that the content plug has been connected
+snap set niryo-sequence seq=1
+print_system "robot configuration applied"
 
-#print_system "network configuration applied"
+while true; do
+    snap refresh niryo-sequence
+    sleep 20
+done
 
 # Mark us done
-touch "$service_dir"/complete
+#touch "$service_dir"/complete
